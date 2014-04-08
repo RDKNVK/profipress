3562d0fe5e4f81969d23a61b2d0bf747e6dd680c
Radek Novak <radek.novak.cze@gmail.com>
2014-04-08 17:24:40 +0200

    bookshelf script made conditional

--- scripts/main.js
+++ scripts/main.js
@@ -4,51 +4,54 @@ $(function() {
         Books.init();
     }
 
-function colorGrad (shelf) {
-	
-    var $colors = $(shelf + " .bk-list li .bk-book .bk-left"), //tady vyber knihy
-        color = $colors.eq(0).css("background-color"),
-        components = color.match(/\d+/g),
-        hsl = rgbToHsl(components[0], components[1], components[2]);
+    function colorGrad(shelf) {
 
-    $colors.each(function(i) {
-        //tady pak uprav to 0.05, ať to co nejvíc odpovídá tomu přechodu v psd
-        $(this).css("background-color", getHslColor(hsl[0], hsl[1], hsl[2] + i * 0.015));
-    });
+        var $colors = $(shelf + " .bk-list li .bk-book .bk-left"), //tady vyber knihy
+            color = $colors.eq(0).css("background-color"),
+            components = color.match(/\d+/g),
+            hsl = rgbToHsl(components[0], components[1], components[2]);
 
-    function getHslColor(h, s, l) {
-        return "hsl(" + (360 * h) + ", " + (100 * s) + "%, " + (100 * l) + "%)";
-    }
+        $colors.each(function(i) {
+            //tady pak uprav to 0.05, ať to co nejvíc odpovídá tomu přechodu v psd
+            $(this).css("background-color", getHslColor(hsl[0], hsl[1], hsl[2] + i * 0.015));
+        });
 
-    function rgbToHsl(r, g, b) {
-        r /= 255, g /= 255, b /= 255;
-        var max = Math.max(r, g, b),
-            min = Math.min(r, g, b);
-        var h, s, l = (max + min) / 2;
-
-        if (max == min) {
-            h = s = 0; // achromatic
-        } else {
-            var d = max - min;
-            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-            switch (max) {
-                case r:
-                    h = (g - b) / d + (g < b ? 6 : 0);
-                    break;
-                case g:
-                    h = (b - r) / d + 2;
-                    break;
-                case b:
-                    h = (r - g) / d + 4;
-                    break;
-            }
-            h /= 6;
+        function getHslColor(h, s, l) {
+            return "hsl(" + (360 * h) + ", " + (100 * s) + "%, " + (100 * l) + "%)";
         }
 
-        return [h, s, l];
+        function rgbToHsl(r, g, b) {
+            r /= 255, g /= 255, b /= 255;
+            var max = Math.max(r, g, b),
+                min = Math.min(r, g, b);
+            var h, s, l = (max + min) / 2;
+
+            if (max == min) {
+                h = s = 0; // achromatic
+            } else {
+                var d = max - min;
+                s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+                switch (max) {
+                    case r:
+                        h = (g - b) / d + (g < b ? 6 : 0);
+                        break;
+                    case g:
+                        h = (b - r) / d + 2;
+                        break;
+                    case b:
+                        h = (r - g) / d + 4;
+                        break;
+                }
+                h /= 6;
+            }
+
+            return [h, s, l];
+        }
     }
-}
-colorGrad(".shelf-1");
-colorGrad(".shelf-2");
-colorGrad(".shelf-3");
+    if ($('.bookshelf').length) {
+        colorGrad(".shelf-1");
+        colorGrad(".shelf-2");
+        colorGrad(".shelf-3");
+
+    }
 });
